fe94af2526d023b23d1da072b78fadfcba449263
Santosh Kumar Dhawal <162580448+GitH05@users.noreply.github.com>
2024-10-08 20:09:51 +0530

    DOM: Document Object Model

---
+++ DOM.js
@@ -0,0 +1,21 @@
+/*
+DOM:Document Objext Model
+-it is structure of the web page as a tree structure:
+-used to decorate the websites.
+        html
+         |
+        body
+        |  |
+       h1  p 
+
+1.Document:
+represent the entire web page and
+serve as the entry point for accessing and manipulating
+the content within the web page.
+
+*/
+document.getElementsByTagName("h6").textContext="Document represent the entire web page";
+document.getElementById("clr");
+clr.style.color="green"; 
+document.querySelector("#clr").innerHTML=`querySelector is a CSS property that is
+"working on the format of the CSS using Js feature"`;
